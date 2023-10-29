76a40f6fb2a805f3a35e368ff9047661b120ac4c
Christo Joseph <1538412+josephch@users.noreply.github.com>
2023-10-29 10:20:13 +0530

    Support json file with relative file path to directory path

--- compile_commands_json_use_symlink.cpp
+++ compile_commands_json_use_symlink.cpp
@@ -30,6 +30,28 @@ SOFTWARE.
 
 #include "nlohmann/json.hpp"
 
+static const char g_file_separator = '/';
+
+static inline void json_item_substitute_file_path_symlink_path(nlohmann::json& item, const std::string realpath_sym,
+															   const std::string& symlink_fullpath) {
+	const std::string& file = item["file"];
+	const std::string& directory = item["directory"];
+	const std::string full_path = directory + g_file_separator + file;
+	char realpath_current[PATH_MAX + 1];
+	if (NULL == realpath(full_path.c_str(), realpath_current)) {
+		std::cerr << "realpath() error. full_path : " << full_path << std::endl;
+	} else {
+		realpath_current[PATH_MAX - 1] = 0;
+		std::string realpath_current_str{realpath_current};
+		if (realpath_current_str.rfind(realpath_sym, 0) == 0) {
+			item["file"] = symlink_fullpath + g_file_separator + realpath_current_str.substr(realpath_sym.length());
+		} else {
+			std::cerr << "No match. full_path : " << full_path << " realpath_current_str : " << realpath_current_str
+					  << std::endl;
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
 	if (argc < 4) {
 		std::cerr << "Usage : " << argv[0] << " <input-json-file> <output-json-file> <path of symlink>" << std::endl;
@@ -37,10 +59,10 @@ int main(int argc, char* argv[]) {
 	}
 	const std::string input = argv[1];
 	const std::string output = argv[2];
+
 	const char* symlink_cstr = argv[3];
 	size_t symlink_start = 0;
 	size_t symlink_end = strlen(symlink_cstr);
-
 	if (symlink_cstr[0] == '.' && symlink_cstr[1] == '/') {
 		symlink_start = 2;
 	}
@@ -59,31 +81,35 @@ int main(int argc, char* argv[]) {
 	inputFile >> json;
 
 	std::string symlink_fullpath;
-	if (symlink.front() != '/') {
+	if (symlink.front() != g_file_separator) {
 		char cwd[PATH_MAX];
 		if (getcwd(cwd, sizeof(cwd)) == NULL) {
 			std::cerr << "getcwd() error" << std::endl;
 			return -1;
 		}
-		symlink_fullpath = std::string(cwd) + "/" + symlink;
+		symlink_fullpath = std::string(cwd) + g_file_separator + symlink;
 	} else {
 		symlink_fullpath = symlink;
 	}
 	std::cout << "symlink_fullpath : " << symlink_fullpath << std::endl;
 
 	char realpath_sym[PATH_MAX];
-	int realpath_len = readlink(symlink_fullpath.c_str(), realpath_sym, sizeof(realpath_sym));
-	if (-1 == realpath_len) {
+	int realpath_sym_len = readlink(symlink_fullpath.c_str(), realpath_sym, sizeof(realpath_sym));
+	if (-1 == realpath_sym_len) {
 		std::cerr << "readlink() error" << std::endl;
 		return -1;
 	}
-	realpath_sym[realpath_len] = 0;
+	realpath_sym[realpath_sym_len] = 0;
 
 	for (nlohmann::json& item : json) {
 		// std::cout << std::setw(4) << item << std::endl << std::endl;
 		const std::string& file = item["file"];
 		if (file.rfind(realpath_sym, 0) == 0) {
-			item["file"] = symlink_fullpath + "/" + file.substr(realpath_len);
+			item["file"] = symlink_fullpath + g_file_separator + file.substr(realpath_sym_len);
+		} else if (file.at(0) != g_file_separator) {
+			json_item_substitute_file_path_symlink_path(item, realpath_sym, symlink_fullpath);
+		} else {
+			std::cerr << "No match. file : " << file << " realpath_sym : " << realpath_sym << std::endl;
 		}
 		// std::cout << std::setw(4) << item << std::endl << std::endl;
 	}
